142134a97ead1915db1d55fb9068ca7e35b9fc56
alicepennec <alicepennec@gmail.com>
2025-12-10 15:32:12 +0100

    génération données de sortie et amélioration TP4

--- TP4/script.py
+++ TP4/script.py
@@ -3,12 +3,12 @@ import os
 import pandas as pd
 from pathlib import Path
 
-csv_path = '/data/fact_resultats_epreuves.csv'
-db_path = '/data/jo_database.duckdb'
-
+csv_path = Path('../data/fact_resultats_epreuves.csv')
+db_path = Path('../data/jo_database.duckdb')
+parquet_dir = Path("../parquet_data")
 
 # Récupération des données CSV
-if not os.path.exists(csv_path):
+if not csv_path.exists():
     raise FileNotFoundError(f"❌ Le fichier {csv_path} est introuvable.")
 
 data = pd.read_csv(csv_path)
@@ -17,9 +17,9 @@ data.info()
 df = data.drop(columns=['id_resultat_source', 'id_athlete_base_resultats', 'id_personne', 'id_equipe', 'id_pays', 'pays_en_base_resultats', 'performance_finale_texte', 'performance_finale', 'id_evenement', 'evenement_en', 'id_edition', 'id_competition_sport', 'id_type_competition', 'id_ville_edition', 'id_nation_edition_base_resultats', 'id_sport', 'sport', 'id_discipline_administrative', 'id_specialite', 'id_epreuve', 'id_federation'])
 
 # Partitionner les données en fonction des années et les exporter sous format Parquet
-Path("../parquet_data").mkdir(exist_ok=True)
+parquet_dir.mkdir(exist_ok=True)
 for annee, subset in df.groupby('edition_saison'):
-    subset.to_parquet(f"../parquet_data/jo_{annee}.parquet", index=False)
+    subset.to_parquet(parquet_dir / f"jo_{annee}.parquet", index=False)
 
 # Connexion à la base DuckDB
 con = duckdb.connect(database=db_path)
